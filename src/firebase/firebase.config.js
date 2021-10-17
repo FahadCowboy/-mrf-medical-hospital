9f6d5a096039d78d8f593f3cbe8135423ca85eca
FahadCowboy <olivierafahad@gmail.com>
2021-10-18 03:04:30 +0600

    initial commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+   apiKey: "AIzaSyAhMcQ4IzxanJOVK5PqKL3rrojnN8QJEzg",
+   authDomain: "mrf-medical-hospital.firebaseapp.com",
+   projectId: "mrf-medical-hospital",
+   storageBucket: "mrf-medical-hospital.appspot.com",
+   messagingSenderId: "923952273516",
+   appId: "1:923952273516:web:d992787149d4e67a00a049"
+ };
+
+ export default firebaseConfig
